6ea1dd56edc4d7740ae134c23674d521b1f4c998
Thomaswang0822 <thomaswang0822@outlook.com>
2024-08-05 20:15:06 -0700

    pour in ScreenSpaceReSTIR and its Pass plugin; endless code refactor ongoing

---
+++ Source/RenderPasses/ScreenSpaceReSTIRPass/ScreenSpaceReSTIRPass.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+ # Copyright (c) 2015-21, NVIDIA CORPORATION. All rights reserved.
+ #
+ # NVIDIA CORPORATION and its licensors retain all intellectual property
+ # and proprietary rights in and to this software, related documentation
+ # and any modifications thereto.  Any use, reproduction, disclosure or
+ # distribution of this software and related documentation without an express
+ # license agreement from NVIDIA CORPORATION is strictly prohibited.
+ **************************************************************************/
+#pragma once
+#include "Falcor.h"
+#include "RenderGraph/RenderPass.h"
+#include "Rendering/ScreenSpaceReSTIR/ScreenSpaceReSTIR.h"
+
+using namespace Falcor;
+
+/** Direct illumination using screen-space ReSTIR.
+
+    This is similar to the SpatiotemporalReservoirResampling pass but uses the
+    ScreenSpaceReSTIR module and serves as an example of how to integrate it.
+*/
+class ScreenSpaceReSTIRPass : public RenderPass
+{
+public:
+    FALCOR_PLUGIN_CLASS(ScreenSpaceReSTIRPass, "ScreenSpaceReSTIRPass",
+        {"Standalone pass for screen-space ReSTIR, which includes ReSTIR DI and GI."})
+
+    static ref<ScreenSpaceReSTIRPass> create(RenderContext* pRenderContext = nullptr, const Dictionary& dict = {});
+
+    ScreenSpaceReSTIRPass(ref<Device> pDevice, const Properties& props);
+
+    virtual Properties getProperties() const override;
+    virtual RenderPassReflection reflect(const CompileData& compileData) override;
+    virtual void compile(RenderContext* pRenderContext, const CompileData& compileData) override;
+    virtual void execute(RenderContext* pRenderContext, const RenderData& renderData) override;
+    virtual void renderUI(Gui::Widgets& widget) override;
+    virtual void setScene(RenderContext* pRenderContext, const ref<Scene>& pScene) override;
+
+    virtual bool onMouseEvent(const MouseEvent& mouseEvent) override;
+    virtual bool onKeyEvent(const KeyboardEvent& keyEvent) override { return false; }
+
+private:
+    void parseProperties(const Properties& props);
+    void recreatePrograms();
+    void prepareSurfaceData(RenderContext* pRenderContext, const ref<Texture>& pVBuffer, int instanceID);
+    void finalShading(RenderContext* pRenderContext, const ref<Texture>& pVBuffer, const RenderData& renderData, int instanceID);
+
+    // Internal state
+    ref<Scene> mpScene;
+
+    std::unique_ptr<ScreenSpaceReSTIR> mpScreenSpaceReSTIR;
+    ScreenSpaceReSTIR::Options mOptions;
+
+    ref<ComputePass> mpPrepareSurfaceDataPass;
+    ref<ComputePass> mpFinalShadingPass;
+
+    uint2 mFrameDim = {0, 0};
+    bool mOptionsChanged = false;
+    bool mGBufferAdjustShadingNormals = false;
+    int mNumReSTIRInstances = 1;
+    bool mNeedRecreateReSTIRInstances = false;
+};
